e408697c49e8b1f30fe3dd7fee9dfa1db74f5434
Young <youngheesomemoto@gmail.com>
2024-01-31 18:46:15 -0600

    console.logged constacts use state

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
+const dummyContacts = [
+  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+];
 
 export default function ContactList() { 
+
+  const [contacts, setContacts] = useState(dummyContacts);
+  console.log("Contacts: ", contacts);
 
   return ( 
         <table>
