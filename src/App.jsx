dbe30e911592892439c6cd853d533f6b52c62b5d
Young <youngheesomemoto@gmail.com>
2024-02-01 18:56:53 -0600

    onclick working

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import ContactList from "./components/ContactList";
-// import SelectedContact from "./components/SelectedContact";
+import SelectedContact from "./components/SelectedContact";
 
 export default function App() {
   const [selectedContactId, setSelectedContactId] = useState(null);
@@ -11,7 +11,7 @@ export default function App() {
       {selectedContactId ? (
         <div>Selected Contact View</div>
       ) : (
-        <ContactList />
+        <ContactList setSelectedContactId={setSelectedContactId}/>
       )}
     </>
   );
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
 import ContactRow from "./ContactRow";
 
-const dummyContacts = [
-  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-];
+// const dummyContacts = [
+//   { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+//   { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+//   { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+// ];
 
 export default function ContactList({ setSelectedContactId }) {
 
-  const [contacts, setContacts] = useState(dummyContacts);
+  const [contacts, setContacts] = useState([]);
 
 
   useEffect(() => {
@@ -43,9 +43,10 @@ export default function ContactList({ setSelectedContactId }) {
           <td>Phone</td>
         </tr>
         {contacts.map((contact) => {
-          return <ContactRow key={contact.id} contact={contact} />;
+          return <ContactRow key={contact.id} 
+          contact={contact} 
+          setSelectedContactId={setSelectedContactId}/>;
         })}
-        
       </tbody>
     </table>
   );
